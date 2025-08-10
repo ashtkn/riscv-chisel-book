96e65b25a995029eae0e38079e1cea78e8f3e2de
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2025-08-10 08:37:55 +0000

    style: remove unnecessary spaces

--- src/main/templates/Memory.scala
+++ src/main/templates/Memory.scala
@@ -26,14 +26,14 @@ class Memory extends Module {
   val mem = Mem(16384, UInt(8.W))
   loadMemoryFromFile(mem, "src/riscv/rv32{isa}-p-{inst}.hex")
   io.imem.inst := Cat(
-    mem(io.imem.addr + 3.U(WORD_LEN.W)), 
+    mem(io.imem.addr + 3.U(WORD_LEN.W)),
     mem(io.imem.addr + 2.U(WORD_LEN.W)),
     mem(io.imem.addr + 1.U(WORD_LEN.W)),
     mem(io.imem.addr)
   )
   io.dmem.rdata := Cat(
     mem(io.dmem.addr + 3.U(WORD_LEN.W)),
-    mem(io.dmem.addr + 2.U(WORD_LEN.W)), 
+    mem(io.dmem.addr + 2.U(WORD_LEN.W)),
     mem(io.dmem.addr + 1.U(WORD_LEN.W)),
     mem(io.dmem.addr)
   )
